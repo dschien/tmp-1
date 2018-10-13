ea0169f480253b40b6fb12af655d71bf52600b8d
Daniel Schien <dance@web.de>
2018-10-13 12:35:24 +0100

    new method: square

--- src/main/java/net/spe/App.java
+++ src/main/java/net/spe/App.java
@@ -6,8 +6,13 @@ package net.spe;
  */
 public class App 
 {
+
+	public static int square(int a){
+		return a + a;
+	}
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
